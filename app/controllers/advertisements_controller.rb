be45af70cc2544ad1f9e4c9eed591971ef2cc533
Brian Liberty <brian.liberty3@gmail.com>
2015-05-14 01:13:02 +0000

    Assignment 27 completed

---
+++ app/controllers/advertisements_controller.rb
@@ -0,0 +1,11 @@
+class AdvertisementsController < ApplicationController
+  
+  def index
+    @advertisements = Advertisement.all
+  end
+  
+  def show
+    @advertisements = Advertisement.find(params[:id])
+  end
+
+end
