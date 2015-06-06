6cd821866c7077aa78df5cfdc0b6d5a5587e17e9
Brian Liberty <brian.liberty3@gmail.com>
2015-06-06 12:20:35 +0000

    Assignment 32 Associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.ordered_by_title
   end
 
   def show
@@ -11,30 +11,30 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-   def create
-     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
-     if @post.save
-       flash[:notice] = "Post was saved."
-       redirect_to @post
-     else
-       flash[:error] = "There was an error saving the post. Please try again."
-       render :new
-     end
-   end
+  def create
+    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
 
   def edit
       @post = Post.find(params[:id])
   end
   
   def update
-     @post = Post.find(params[:id])
-     if @post.update_attributes(params.require(:post).permit(:title, :body))
-       flash[:notice] = "Post was updated."
-       redirect_to @post
-     else
-       flash[:error] = "There was an error saving the post. Please try again."
-       render :edit
-     end
-   end
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
   
 end
