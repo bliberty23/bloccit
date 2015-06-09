81ca2165df50b0be9ae0a7db7bf26aac73b5626b
Xander Miller <xander.miller@gmail.com>
2015-06-09 20:51:38 +0000

    refactors seed file and adds admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,59 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
 
- require 'faker'
- 
- # Create Users
- 5.times do
-   user = User.new(
-     name:     Faker::Name.name,
-     email:    Faker::Internet.email,
-     password: Faker::Lorem.characters(10)
-   )
-   user.skip_confirmation!
-   user.save!
- end
- users = User.all
- 
- # Note: by calling `User.new` instead of `create`,
- # we create an instance of User which isn't immediately saved to the database.
- 
- # The `skip_confirmation!` method sets the `confirmed_at` attribute
- # to avoid triggering an confirmation email when the User is saved.
- 
- # The `save` method then saves this User to the database.
- 
- # Create Posts
- 50.times do
-   Post.create!(
-     user:   users.sample,
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
- 
- # Create Comments
- 100.times do
-   Comment.create!(
-    # user: users.sample,   # we have not yet associated Users with Comments
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
- 
- user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'brian.liberty3@gmail.com',
-   password: 'helloworld'
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+
+unless User.find_by_email 'brian.liberty3@gmail.com'
+  dev = User.new(
+    name: "Brian Liberty",
+    email: 'brian.liberty3@gmail.com',
+    password: 'helloworld'
+  )
+  dev.skip_confirmation!
+  dev.save!
+end
+
+unless User.find_by_email 'admin@example.com'
+  admin = User.new(
+    name: "Optimus Prime",
+    email: 'admin@example.com',
+    #role: "admin",
+    password: 'helloworld'
+  )
+  admin.skip_confirmation!
+  admin.save!
+end
+
+users = User.all
+
+50.times do
+ Post.create!(
+   user:   users.sample,
+   title:  Faker::Lorem.sentence,
+   body:   Faker::Lorem.paragraph
  )
- 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+end
+posts = Post.all
+
+# Create Comments
+100.times do
+ Comment.create!(
+  # user: users.sample,   # we have not yet associated Users with Comments
+   post: posts.sample,
+   body: Faker::Lorem.paragraph
+ )
+end
+
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
