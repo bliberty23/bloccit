65cf67514e670d844a2b7f1550e90251d0b606db
Xander Miller <xander.miller@gmail.com>
2015-06-10 14:53:33 +0000

    adjusts assignment to meet requirements

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -3,6 +3,8 @@
     def resolve
       if user.admin?
         scope.all
+      elsif user.present?
+        scope.where(user_id: user.id)
       else
         scope.where(:published => true)
       end
