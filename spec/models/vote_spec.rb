6a029650a8ad40737dac953b060bc40e7e6a014f
Brian Liberty <brian.liberty3@gmail.com>
2015-07-24 04:35:50 +0000

    Checkpoint 49 Voting, complete

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Vote do
+    describe "validations" do
+        describe "value validation" do
+            it "only allows -1 or 1 as values" do
+               v_up = Vote.new(value: 1)
+               expect( v_up.valid? ).to eq(true)
+         
+                v_down = Vote.new(value: -1)
+                expect( v_down.valid? ).to eq(true)
+             
+                no_vote = Vote.new(value: 2)
+                expect( no_vote.valid? ).to eq(false)
+            end
+        end
+    end
+end 
+
