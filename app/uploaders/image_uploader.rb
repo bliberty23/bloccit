1dca3e7441aae019de6b19ef22c3982ea00dae37
Xander Miller <xander.miller@gmail.com>
2015-07-31 04:42:49 +0000

    fixes image uploading

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,11 +4,10 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -25,7 +24,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :scale => [512, 512]
+  process :resize_to_fill => [512, 512]
   #
   # def scale(width, height)
   #   # do something
@@ -33,7 +32,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_fit => [64, 64]
+     process :resize_to_fill => [64, 64]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
